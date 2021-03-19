91d2067f36432f9428ccffb7da2f3916c25a3629
Emma Vukelj <emma.vukelj@gmail.com>
2021-03-19 13:03:53 -0400

    Add isrm

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 INMAP_SANDBOX_ROOT=$(pwd)
 INMAP_ROOT_DIR=/Users/emmavukelj/evookelj_inmap
+SLCASpatialCache=/Users/emmavukelj/.SLCASpatialCache
+nei2014Dir=/Users/emmavukelj/nei2014
 export INMAP_SANDBOX_ROOT
 export INMAP_ROOT_DIR
+export SLCASpatialCache
+export nei2014Dir
+
+# Download https://zenodo.org/record/3534712/files/isrm_v1.2.1.zip?download=1 into $(INMAP_SANDBOX_ROOT)/data and unzip
